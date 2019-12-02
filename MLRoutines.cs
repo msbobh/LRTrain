755696be229d575711cdbf423428ee6fdf6eb090
Bob Hildreth <bobh@thehildreths.com>
2019-12-01 17:55:57 -0800

    Created a separate .cs for Learning Routines to get them out of the main program Added a routine using BFGS Added code to save the model files with the training filename .<model type>.save

---
+++ MLRoutines.cs
@@ -0,0 +1,62 @@
+﻿using Accord.MachineLearning;
+using Accord.Statistics;
+using Accord.Statistics.Models.Regression.Fitting;
+using Accord.Statistics.Models.Regression;
+using Accord.Math.Optimization;
+using Accord.IO;
+
+
+namespace LearningRountines
+{
+    class MLAlgorithms
+    {
+
+        static public bool [] IterativeLeastSquares(double[][] input1, double[] output1, string fName)
+        {
+            
+            var learner = new IterativeReweightedLeastSquares<LogisticRegression>()
+            {
+                // Gets or sets the tolerance value used to determine whether the algorithm has converged.
+                Tolerance = 1e-4,  // Let's set some convergence parameters
+                MaxIterations = 100,  // maximum number of iterations to perform
+                Regularization = 0
+            };
+
+            // Now, we can use the learner to finally estimate our model:
+            LogisticRegression regression = learner.Learn(input1, output1);
+
+            double[] scores = regression.Probability(input1);
+            
+            regression.Save(fName.Replace(".csv", ".IRLS.save"), compression: SerializerCompression.None);
+
+            // Finally, if we would like to arrive at a conclusion regarding
+            // each sample, we can use the Decide method, which will transform
+            // the probabilities (from 0 to 1) into actual true/false values:
+
+            return regression.Decide(input1);
+            // mean(double(p == y)) * 100);
+        }
+
+        static public int[] ConjugateGradientDescentBFGS (double [][] input, int [] labels, string fName)
+        {
+            // Create a Conjugate Gradient algorithm to estimate the regression
+            var mlbfgs = new MultinomialLogisticLearning<BroydenFletcherGoldfarbShanno>();
+            
+            // Now, we can estimate our model using BFGS
+            MultinomialLogisticRegression mlr = mlbfgs.Learn(input, labels);
+
+            // We can compute the model answers
+            int[] answers = mlr.Decide(input);
+            
+            // And also the probability of each of the answers
+            double[][] probabilities = mlr.Probabilities(input);
+
+            // Now we can check how good our model is at predicting
+            double error = new Accord.Math.Optimization.Losses.ZeroOneLoss(labels).Loss(answers);
+            mlr.Save(fName, compression: SerializerCompression.None);
+
+            return answers;
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Accord.Math;
 using Accord.IO;
 using Funcs;
 using resources;
+using Accord.MachineLearning.VectorMachines.Learning;
 
 
 namespace AccordLogisticRegression
@@ -75,61 +76,68 @@ namespace AccordLogisticRegression
             // Apparantly if import direclty to Jagged from the csv Accord does not parse the data correctly.
             // For Accord.net Logistic Regression the input data needs to be in Jagged Arrays         
             double [][] input1 = Funcs.Utility.convertToJaggedArray(MatrixIn);
+            
             // Labels can either be int (1,0) or bools
             int [] output1 = Utility.convetToJaggedArray(labels);
-                    
+            // ********************* new stuff to try out *********************
 
-            var learner = new IterativeReweightedLeastSquares<LogisticRegression>()
+            var teacher = new Accord.MachineLearning.VectorMachines.Learning.ProbabilisticCoordinateDescent()
             {
-                                   // Gets or sets the tolerance value used to determine whether the algorithm has converged.
-                Tolerance = 1e-4,  // Let's set some convergence parameters
-                MaxIterations = 100,  // maximum number of iterations to perform
-                Regularization = 0
+                Tolerance = 1e-10,
+                Complexity = 1e+10, // learn a hard-margin model
             };
-
-            // Now, we can use the learner to finally estimate our model:
-            LogisticRegression regression = learner.Learn(input1, output1);
-
-
+            var svm = teacher.Learn(input1, output1);
+            var svmregression = (LogisticRegression)svm;
+                   
             // Write the model out to a save file
-            string modelsavefilename = trainingfile.Replace(".csv", ".save");
-            double[] scores = regression.Probability(input1);
+            string modelsavefilename = trainingfile.Replace(".csv", ".PCD.save");
             
-            regression.Save(modelsavefilename, compression: SerializerCompression.None);
+            svmregression.Save(modelsavefilename, compression: SerializerCompression.None);
 
-            // Finally, if we would like to arrive at a conclusion regarding
-            // each sample, we can use the Decide method, which will transform
+            // Use the Decide method, to  transform
             // the probabilities (from 0 to 1) into actual true/false values:
-
-            bool [] actual = regression.Decide(input1);
-            // mean(double(p == y)) * 100);
-
-            double[,] learnedParams = Utility.converRowtoColumn( regression.Weights);
-
-            string filename = "LearnedParams.csv";
-                                 
+            bool[] predicted = svmregression.Decide(input1);
             
-            using (CsvWriter writer = new CsvWriter(filename))
-            {
-                writer.Write(learnedParams);
-            }
-            
-            int[] predictions = Utility.BoolToInt(actual);           
+
+            int[] svmpredicts = Utility.BoolToInt(predicted); // Decide returns boolean need to conver to ints to compare w/ input labels
             double subtotal = 0;
             int index = 0;
-            foreach (var result in predictions)
+            foreach (var result in svmpredicts)
             {
-                if(result == output1[index])
+                if (result == output1[index])
                 {
                     subtotal = subtotal + 1;
                 }
                 index++;
             }
-            double accuracy =  subtotal / predictions.Count();
-            Console.WriteLine("Predicted accuracy:{0}", Math.Round(accuracy * 100, 2));
-            
+            double svmaccuracy = subtotal / svmpredicts.Count();
+            Console.WriteLine("SVM Accuracy:{0}", Math.Round(svmaccuracy * 100, 2));
+            // Compute the classification error as in SVM example
+            double error = new Accord.Math.Optimization.Losses.ZeroOneLoss(output1).Loss(predicted);
+            Console.WriteLine("Zero One Loss:{0}", Math.Round(error, 2));
+            /*
+             Console.WriteLine("Predicted accuracy:{0}", Math.Round(accuracy * 100, 2));
+             */
+             /*
+              * Run the same data through a BFGS optimizaiton routine and save the model
+              * 
+              */
 
-                
+            int[] BFGSResults = LearningRountines.MLAlgorithms.ConjugateGradientDescentBFGS(input1, output1, trainingfile.Replace(".csv", ".BFGS.save"));
+            index = 0;
+            subtotal = 0;
+            foreach (var result in BFGSResults)
+            {
+                if (result == output1[index])
+                {
+                    subtotal = subtotal + 1;
+                }
+                index++;
+            }
+            double BFGSAccuracy = subtotal / BFGSResults.Count();
+            Console.WriteLine("BFGS Accuracy => {0}", Math.Round(BFGSAccuracy * 100, 2));
+
+
         }
     }
 }

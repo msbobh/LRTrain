d3d27df9ee86e3a745514ead77d990f1f3c6ba9a
Bob Hildreth <bobh@thehildreths.com>
2020-03-20 18:52:39 -0700

    Missed file in previous commit

--- MLRoutines.cs
+++ MLRoutines.cs
@@ -5,6 +5,8 @@ using Accord.IO;
 using Accord.Statistics.Analysis; // Needed for confusion matrix
 using Accord.Math.Optimization.Losses; // needed for use of Zero One Loss routine
 using Accord.MachineLearning; // Needed for cross validation
+using System;
+using Accord.MachineLearning.VectorMachines.Learning;
 
 namespace LearningRountines
 {
@@ -69,7 +71,7 @@ namespace LearningRountines
                 fit: (teacher, x, y, w) => teacher.Learn(x, y, w),
                 x: input, y: labels);
             var result = cv.Learn(input, labels);
-
+            
             System.Console.WriteLine(   "Cross Validation Mean {0}, Training Error {1}", result.Validation.Mean, result.Training.Variance);
 
             GeneralConfusionMatrix gcm = result.ToConfusionMatrix(input, labels);
@@ -87,8 +89,7 @@ namespace LearningRountines
 
             return answers;
         }
-        
-        static public  int[] ProbabilisticCoordinetDescent( double[][] input1, int[] labels, string SaveFile)
+        static public  int[] ProbabilisticCoordinateDescent( double[][] input1, int[] labels, string SaveFile)
         {
             // http://accord-framework.net/docs/html/T_Accord_MachineLearning_VectorMachines_Learning_ProbabilisticCoordinateDescent.htm
             /* This class implements a SupportVectorMachine learning algorithm specifically crafted for
@@ -100,10 +101,42 @@ namespace LearningRountines
              * regression (probabilistic svm) problems.
              */
 
-            var teacher = new Accord.MachineLearning.VectorMachines.Learning.ProbabilisticCoordinateDescent()
+            int folds = 5;
+            Accord.Math.Random.Generator.Seed = 0;
+            var cv = CrossValidation.Create(
+
+                    k: folds, // We will be using 10-fold cross validation
+
+                    // First we define the learning algorithm:
+                    learner: (p) => new ProbabilisticCoordinateDescent(),
+
+                    // Now we have to specify how the n.b. performance should be measured:
+                    loss: (actual, expected, p) => new ZeroOneLoss(expected).Loss(actual),
+
+                    // This function can be used to perform any special
+                    // operations before the actual learning is done, but
+                    // here we will just leave it as simple as it can be:
+                    fit: (teach, x, y, w) => teach.Learn(x, y, w),
+
+                    // Finally, we have to pass the input and output data
+                    // that will be used in cross-validation. 
+                    x: input1, y: labels
+                    );
+            
+            var cvresult = cv.Learn(input1, labels);
+            Console.WriteLine("  Generating a cross validation for the dataset");
+            Console.WriteLine("  Cross Validation samples {0}", cvresult.NumberOfSamples);
+            Console.WriteLine("  Cross Validation features {0}", cvresult.NumberOfInputs);
+            Console.WriteLine("  Cross Validation Training Mean {0}", cvresult.Training.Mean);
+            GeneralConfusionMatrix gcm = cvresult.ToConfusionMatrix(input1, labels);
+            Console.WriteLine("  Confusion Matrix Accuracy {0}", gcm.Accuracy);
+
+            
+            var teacher = new ProbabilisticCoordinateDescent()
             {
                 Tolerance = 1e-10,
-                Complexity = 1e+10, // learn a hard-margin model
+                Complexity = 1e+10,
+                // learn a hard-margin model
                 /* Complexity (cost) parameter C. Increasing the value of C forces the creation of a more
                  * accurate model that may not generalize well. If this value is not set and UseComplexityHeuristic
                  * is set to true, the framework will automatically guess a value for C. If this value is manually
@@ -113,6 +146,12 @@ namespace LearningRountines
             };
             var svm = teacher.Learn(input1, labels);
             var svmregression = (LogisticRegression)svm;
+            ConfusionMatrix cm = ConfusionMatrix.Estimate(svm, input1, labels);
+            // accuracy, TP, FP, FN, TN and FScore Diagonal
+
+            Console.WriteLine(" Results of Training run");
+            Console.WriteLine(" Accuracy:{0}, False Positives: {1}, False Negatives: {2}, Fscore: {3}\n", cm.Accuracy,
+                cm.FalsePositives, cm.FalseNegatives, cm.FScore);
 
             // Write the model out to a save file
             string modelsavefilename = SaveFile.Replace(".csv", ".PCD.save");

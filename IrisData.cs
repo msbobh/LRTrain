2339d9a2654b8d57033431c0a11cfeb4bca578df
Bob Hildreth <bobh@thehildreths.com>
2023-10-09 17:46:42 -0700

    Missed addgn this the frist tiem, imports IRIS data for machine learning.

---
+++ IrisData.cs
@@ -0,0 +1,26 @@
+﻿using Accord.DataSets;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.MachineLearning.VectorMachines;
+using Accord.Statistics.Analysis;
+using Accord.Statistics.Kernels;
+using Accord.Math.Random;
+using System;
+
+namespace LRTrain
+{
+    class IrisData
+    {
+        public IrisData (in int rando)
+        {
+            // Generate always same random numbers
+           Generator.Seed = rando;
+
+            // Download and load the Iris dataset
+            Iris iris = new Iris();
+            double[][] inputs = iris.Instances;
+            int[] outputs = iris.ClassLabels;           
+
+        }
+        
+    }
+}
